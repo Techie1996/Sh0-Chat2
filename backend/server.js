f74926609c7b0d99f0822b8164ed15bfeb747741
Govinda <fearlessjack00@gmail.com>
2023-09-22 11:48:09 +0530

    Deployment Section

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddlewares");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -16,14 +17,28 @@ const app = express();
 app.use(express.json());
 // Very First Expressjs Api Is Here
 
-app.get("/",(req,res)=>{
-res.send("API Is Running");
-});
-
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 //A Little More Complex API
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
